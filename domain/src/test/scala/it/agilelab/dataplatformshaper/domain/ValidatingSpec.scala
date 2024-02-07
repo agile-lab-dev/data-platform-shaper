d7bc9d734aa03f19311bfd56001d4ccff2c46a6f
pusceddusimone <92328763+pusceddusimone@users.noreply.github.com>
2024-02-07 10:33:36 +0100

    Added tests for validation

--- domain/src/test/scala/it/agilelab/dataplatformshaper/domain/ValidatingSpec.scala
+++ domain/src/test/scala/it/agilelab/dataplatformshaper/domain/ValidatingSpec.scala
@@ -7,13 +7,21 @@ import it.agilelab.dataplatformshaper.domain.model.schema.Mode.*
 import org.scalactic.Equality
 import org.scalatest.BeforeAndAfterAll
 import cats.effect.std.Random
+import cats.data.EitherT
 import cats.effect.testing.scalatest.AsyncIOSpec
 import org.http4s.multipart.{Multipart, Multiparts, Part}
 import org.http4s.{EntityEncoder, Method, Request, Uri}
 import fs2.io.file.Path
 import org.http4s.ember.client.EmberClientBuilder
-import it.agilelab.dataplatformshaper.domain.knowledgegraph.interpreter.{Rdf4jKnowledgeGraph, Session}
-import it.agilelab.dataplatformshaper.domain.service.interpreter.{TraitManagementServiceInterpreter, TypeManagementServiceInterpreter}
+import it.agilelab.dataplatformshaper.domain.knowledgegraph.interpreter.{
+  Rdf4jKnowledgeGraph,
+  Session
+}
+import it.agilelab.dataplatformshaper.domain.service.interpreter.{
+  InstanceManagementServiceInterpreter,
+  TraitManagementServiceInterpreter,
+  TypeManagementServiceInterpreter
+}
 import it.agilelab.dataplatformshaper.domain.model.l0
 import it.agilelab.dataplatformshaper.domain.model.l0.*
 
@@ -23,11 +31,17 @@ import org.scalatest.matchers.should.Matchers
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 import cats.effect.{IO, Ref}
+import it.agilelab.dataplatformshaper.domain.service.ManagementServiceError
+import it.agilelab.dataplatformshaper.domain.service.ManagementServiceError.ValidationError
 
 import java.time.{LocalDate, ZoneId, ZonedDateTime}
 import scala.language.{dynamics, implicitConversions}
 
-class ValidatingSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with BeforeAndAfterAll:
+class ValidatingSpec
+    extends AsyncFreeSpec
+    with AsyncIOSpec
+    with Matchers
+    with BeforeAndAfterAll:
 
   val graphdbType = "graphdb"
 
@@ -146,14 +160,14 @@ class ValidatingSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with B
             Repeated
           )
         )
-      ),
-      "columns" -> StructType(
+      )
+      /*"columns" -> StructType(
         List(
           "name" -> StringType(),
           "type" -> StringType()
         ),
         Repeated
-      )
+      )*/
     )
   )
 
@@ -217,19 +231,152 @@ class ValidatingSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with B
       ("nest3" -> "ciccio3", "nest4" -> "ciccio4"),
       ("nest3" -> "ciccio5", "nest4" -> "ciccio6")
     )),
-    "columns" -> List(
+    /*"columns" -> List(
       ("name" -> "FirstName", "type" -> "String"),
       ("name" -> "FamilyNane", "type" -> "String"),
       ("name" -> "Age", "type" -> "Int")
-    )
+    )*/
   )
 
+  val nonConformingTuple: Tuple = (
+    "organization" -> "HR",
+    "sub-organization" -> "Any",
+    "domain" -> "Registration",
+    "sub-domain" -> "Person",
+    "foundation" -> LocalDate.of(2008, 8, 26),
+    "foundationRepeated" -> List(
+      LocalDate.of(2008, 8, 26),
+      LocalDate.of(1966, 11, 24)
+    ),
+    "foundationNullable" -> None,
+    "aTimestamp" -> ZonedDateTime.of(
+      2023,
+      10,
+      11,
+      12,
+      0,
+      0,
+      0,
+      ZoneId.of("Europe/London")
+    ),
+    "aTimestampRepeated" -> List(
+      ZonedDateTime.of(2022, 10, 11, 12, 1, 12, 13, ZoneId.of("Europe/London")),
+      ZonedDateTime.of(2023, 10, 11, 12, 0, 0, 0, ZoneId.of("Europe/London"))
+    ),
+    "aTimestampNullable" -> None,
+    "aDouble" -> 1.23,
+    "aDoubleRepeated" -> List(1.23, 3.21),
+    "aDoubleNullable" -> Some(3.21),
+    "aJson" -> parse(
+      "{\"name\": \"Michael Johnson\", \"age\": 33, \"city\": \"Los Angeles\"}"
+    ).getOrElse(""),
+    "aJsonRepeated" -> List(
+      parse(
+        "{\"name\": \"Alice Williams\", \"age\": 29, \"city\": \"San Francisco\"}"
+      ).getOrElse(""),
+      parse(
+        "{\"name\": \"Robert Brown\", \"age\": 45, \"city\": \"Chicago\"}"
+      ).getOrElse("")
+    ),
+    "aJsonNullable" -> Some(
+      parse(
+        "{\"name\": \"Olivia Davis\", \"age\": 31, \"city\": \"Houston\"}"
+      ).getOrElse("")
+    ),
+    "aFloat" -> 1.23f,
+    "aFloatRepeated" -> List(1.23f, 3.21f),
+    "aFloatNullable" -> Some(1.23f),
+    "aLong" -> 5L,
+    "aLongRepeated" -> List(0L, 3L),
+    "aLongNullable" -> Some(3L),
+    "aBool" -> true,
+    "aBoolRepeated" -> List(true, false),
+    "aBoolNullable" -> Some(false),
+    "labels" -> List("label1", "label2"),
+    "aIntNullable" -> Some(1),
+    "nested" -> ("nest1" -> "ciccio1", "nest2" -> "ciccio2", "furtherNested" -> List(
+      ("nest3" -> "ciccio3", "nest4" -> "ciccio4"),
+      ("nest3" -> "ciccio5", "nest4" -> "ciccio6")
+    )),
+    /*"columns" -> List(
+      ("name" -> "FirstName", "type" -> "String"),
+      ("name" -> "FamilyNane", "type" -> "String"),
+      ("name" -> "Age", "type" -> "Int")
+    )*/
+  )
+
+  val conformingTupleForUpdate: Tuple = (
+    "organization" -> "HR",
+    "sub-organization" -> "Any",
+    "domain" -> "Registration",
+    "sub-domain" -> "Person",
+    "foundation" -> LocalDate.of(2008, 8, 26),
+    "foundationRepeated" -> List(
+      LocalDate.of(2008, 8, 26),
+      LocalDate.of(1966, 11, 24)
+    ),
+    "foundationNullable" -> None,
+    "aTimestamp" -> ZonedDateTime.of(
+      2023,
+      10,
+      11,
+      12,
+      0,
+      0,
+      0,
+      ZoneId.of("Europe/London")
+    ),
+    "aTimestampRepeated" -> List(
+      ZonedDateTime.of(2022, 10, 11, 12, 1, 12, 13, ZoneId.of("Europe/London")),
+      ZonedDateTime.of(2023, 10, 11, 12, 0, 0, 0, ZoneId.of("Europe/London"))
+    ),
+    "aTimestampNullable" -> None,
+    "aDouble" -> 1.23,
+    "aDoubleRepeated" -> List(1.23, 3.21),
+    "aDoubleNullable" -> Some(3.21),
+    "aJson" -> parse(
+      "{\"name\": \"Michael Johnson\", \"age\": 33, \"city\": \"Los Angeles\"}"
+    ).getOrElse(""),
+    "aJsonRepeated" -> List(
+      parse(
+        "{\"name\": \"Alice Williams\", \"age\": 29, \"city\": \"San Francisco\"}"
+      ).getOrElse(""),
+      parse(
+        "{\"name\": \"Robert Brown\", \"age\": 45, \"city\": \"Chicago\"}"
+      ).getOrElse("")
+    ),
+    "aJsonNullable" -> Some(
+      parse(
+        "{\"name\": \"Olivia Davis\", \"age\": 31, \"city\": \"Houston\"}"
+      ).getOrElse("")
+    ),
+    "aFloat" -> 1.23f,
+    "aFloatRepeated" -> List(1.23f, 3.21f),
+    "aFloatNullable" -> Some(1.23f),
+    "aLong" -> 1L,
+    "aLongRepeated" -> List(1L, 3L),
+    "aLongNullable" -> Some(3L),
+    "aBool" -> true,
+    "aBoolRepeated" -> List(true, false),
+    "aBoolNullable" -> Some(false),
+    "labels" -> List("label1", "label2"),
+    "aIntNullable" -> Some(1),
+    "nested" -> ("nest1" -> "ciccio1", "nest2" -> "ciccio2", "furtherNested" -> List(
+      ("nest3" -> "ciccio3", "nest4" -> "ciccio4"),
+      ("nest3" -> "ciccio5", "nest4" -> "ciccio6")
+    )),
+    /*"columns" -> List(
+      ("name" -> "FirstName", "type" -> "String"),
+      ("name" -> "FamilyNane", "type" -> "String"),
+      ("name" -> "Age", "type" -> "Int")
+    )*/
+  )
 
   "Unfolding a tuple conform to a schema" - {
     "should work" in {
       val res = cueValidate(schema, tuple)
       res match {
-        case Right(_) => succeed
+        case Right(_)     => succeed
         case Left(errors) => fail(s"Validation failed with errors: $errors")
       }
     }
@@ -259,7 +406,152 @@ class ValidatingSpec extends AsyncFreeSpec with AsyncIOSpec with Matchers with B
       } asserting (ret =>
         ret should matchPattern { case Right(_) =>
         }
+      )
+    }
+  }
+
+  "Creating a conforming instance for an EntityType" - {
+    "works" in {
+      val session = Session[IO](
+        graphdbType,
+        "localhost",
+        7201,
+        "dba",
+        "mysecret",
+        "repo1",
+        false
+      )
+      session.use { session =>
+        val repository = Rdf4jKnowledgeGraph[IO](session)
+        val trservice = new TraitManagementServiceInterpreter[IO](repository)
+        val tservice = new TypeManagementServiceInterpreter[IO](trservice)
+        val iservice = new InstanceManagementServiceInterpreter[IO](tservice)
+        iservice.create(
+          "ValidationDataCollectionType",
+          tuple
         )
+      } asserting (_ should matchPattern { case Right(_) => })
+    }
+  }
+
+  "Creating a non-conforming instance for an EntityType" - {
+    "fails" in {
+      val session = Session[IO](
+        graphdbType,
+        "localhost",
+        7201,
+        "dba",
+        "mysecret",
+        "repo1",
+        false
+      )
+      session.use { session =>
+        val repository = Rdf4jKnowledgeGraph[IO](session)
+        val trservice = new TraitManagementServiceInterpreter[IO](repository)
+        val tservice = new TypeManagementServiceInterpreter[IO](trservice)
+        val iservice = new InstanceManagementServiceInterpreter[IO](tservice)
+        iservice.create(
+          "ValidationDataCollectionType",
+          nonConformingTuple
+        )
+      } asserting (_ should matchPattern {
+        case Left(ValidationError(errors)) if errors.size.equals(4) =>
+      })
+    }
+  }
+
+  "Updating an Entity with conforming values" - {
+    "works" in {
+      val session = Session[IO](
+        graphdbType,
+        "localhost",
+        7201,
+        "dba",
+        "mysecret",
+        "repo1",
+        false
+      )
+      session.use { session =>
+        val repository = Rdf4jKnowledgeGraph[IO](session)
+        val trservice = new TraitManagementServiceInterpreter[IO](repository)
+        val tservice = new TypeManagementServiceInterpreter[IO](trservice)
+        val iservice = new InstanceManagementServiceInterpreter[IO](tservice)
+        (for {
+          uid <- EitherT[IO, ManagementServiceError, String](
+            iservice.create(
+              "ValidationDataCollectionType",
+              tuple
+            )
+          )
+          _ <- EitherT[IO, ManagementServiceError, String](
+            iservice.update(
+              uid,
+              conformingTupleForUpdate
+            )
+          )
+          read <- EitherT[IO, ManagementServiceError, Entity](
+            iservice.read(uid)
+          )
+        } yield read).value
+      } asserting (entity => {
+        entity should matchPattern {
+          case Right(Entity(_, "ValidationDataCollectionType", _)) =>
+        }
+        entity match {
+          case Right(Entity(_, _, data)) =>
+            val x =
+              tupleToJsonChecked(data, schema)
+            val y = tupleToJsonChecked(
+              conformingTupleForUpdate,
+              schema
+            )
+            x shouldBe y
+          case _ => fail("Unexpected pattern encountered")
+        }
+      })
+    }
+  }
+
+  "Updating an Entity with non-conforming values" - {
+    "fails" in {
+      val session = Session[IO](
+        graphdbType,
+        "localhost",
+        7201,
+        "dba",
+        "mysecret",
+        "repo1",
+        false
+      )
+      session.use { session =>
+        val repository = Rdf4jKnowledgeGraph[IO](session)
+        val trservice = new TraitManagementServiceInterpreter[IO](repository)
+        val tservice = new TypeManagementServiceInterpreter[IO](trservice)
+        val iservice = new InstanceManagementServiceInterpreter[IO](tservice)
+        (for {
+          uid <- EitherT[IO, ManagementServiceError, String](
+            iservice.create(
+              "ValidationDataCollectionType",
+              tuple
+            )
+          )
+          update <- EitherT[IO, ManagementServiceError, String](
+            iservice.update(
+              uid,
+              nonConformingTuple
+            )
+          )
+        } yield update).value
+      } asserting {
+        case Left(ValidationError(errors)) =>
+          withClue(
+            "Update should fail with ValidationError containing specific errors: "
+          ) {
+            errors.size shouldBe 4
+          }
+        case _ =>
+          fail("Expected ValidationError but received a different result")
+      }
     }
   }
 
