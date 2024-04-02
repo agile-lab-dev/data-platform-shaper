95744e867dc4f5eb292ccdd1cc63a4cac2b70cd9
David Greco <greco@acm.org>
2024-04-02 10:07:58 +0200

    #71 Make the traits inheritable
    Co-authored-by: David Greco <d.greco@icloud.com>

--- domain/src/test/scala/it/agilelab/dataplatformshaper/domain/EntityTypeSpec.scala
+++ domain/src/test/scala/it/agilelab/dataplatformshaper/domain/EntityTypeSpec.scala
@@ -10,35 +10,35 @@ class EntityTypeSpec extends AnyFlatSpec with Matchers:
 
   behavior of "EntityType"
 
-  "Inheritance and traits" should "work" in {
-
-    val schema0: Schema = StructType(
-      List(
-        "field0" -> StringType()
-      )
+  val schema0: Schema = StructType(
+    List(
+      "field0" -> StringType()
     )
+  )
 
-    val schema1: Schema = StructType(
-      List(
-        "field1" -> StringType(),
-        "field3" -> StringType()
-      )
+  val schema1: Schema = StructType(
+    List(
+      "field1" -> StringType(),
+      "field3" -> StringType()
     )
+  )
 
-    val schema2: Schema = StructType(
-      List(
-        "field1" -> StringType(),
-        "field2" -> StringType(),
-        "field3" -> StringType(),
-        "field4" -> StringType(),
-        "field5" -> DateType(),
-        "field6" -> TimestampType(),
-        "field7" -> DoubleType(),
-        "field8" -> FloatType(),
-        "field9" -> LongType(),
-        "field10" -> BooleanType()
-      )
+  val schema2: Schema = StructType(
+    List(
+      "field1" -> StringType(),
+      "field2" -> StringType(),
+      "field3" -> StringType(),
+      "field4" -> StringType(),
+      "field5" -> DateType(),
+      "field6" -> TimestampType(),
+      "field7" -> DoubleType(),
+      "field8" -> FloatType(),
+      "field9" -> LongType(),
+      "field10" -> BooleanType()
     )
+  )
+
+  "Schema inheritance" should "work" in {
 
     val entityType0 = EntityType("EntityType0", schema0)
 
@@ -61,6 +61,24 @@ class EntityTypeSpec extends AnyFlatSpec with Matchers:
         "field10" -> BooleanType()
       )
     )
+
+  }
+
+  "Traits inheritance" should "work" in {
+
+    val entityType0 = EntityType("EntityType0", Set("Trait1"), schema0)
+
+    val entityType1 = l0.EntityType(
+      "EntityType1",
+      Set("Trait1", "Trait2"),
+      schema1,
+      entityType0
+    )
+
+    val entityType2 =
+      l0.EntityType("EntityType2", Set("Trait3"), schema2, entityType1)
+
+    entityType2.traits.shouldBe(Set("Trait1", "Trait2", "Trait3"))
 
   }
 
