095857f3243981e5431e11d2d2f8773c4bbf214b
pusceddusimone <92328763+pusceddusimone@users.noreply.github.com>
2024-05-19 12:53:56 +0200

    Fixed create mapping with yaml
    The additional references were missing

--- uservice/src/main/scala/it/agilelab/dataplatformshaper/uservice/api/intepreter/OntologyManagerHandler.scala
+++ uservice/src/main/scala/it/agilelab/dataplatformshaper/uservice/api/intepreter/OntologyManagerHandler.scala
@@ -1229,7 +1229,8 @@ class OntologyManagerHandler[F[_]: Async](
                 body.mappingKey.sourceEntityTypeName,
                 body.mappingKey.targetEntityTypeName
               ),
-              tuple
+              tuple,
+              body.additionalSourcesReferences
             )
           )
           .map(_.leftMap(err => Vector(err.errors.head)))
@@ -1254,7 +1255,7 @@ class OntologyManagerHandler[F[_]: Async](
     res
       .map {
         case Left(errors) => respond.BadRequest(ValidationError(errors))
-        case Right(())    => respond.Ok("Mapping created successfully")
+        case Right(())    => respond.Ok("Mapped instances created successfully")
       }
       .onError(t =>
         Applicative[F].pure(logger.error(s"Error: ${t.getMessage}"))
