7a3268563d4504c6a01c6ac2379fd97899226cbf
Roland Tritsch <roland@tritsch.email>
2024-07-29 06:48:23 +0100

    Disable build trace. It's noicy.

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -2,7 +2,7 @@
 
 set -o errexit # exit when a command fails
 # set -o nounset # exit when a script tries to use undeclared variables
-set -o xtrace # trace what gets executed
+# set -o xtrace # trace what gets executed
 
 # This script calculates the current version based on the current branch and on the existing tags:
 # - For tags (in the form v1.0.2) the version returned is always a stable one; in the previous example it would return 1.0.2.
